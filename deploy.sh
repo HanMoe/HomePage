8378f1aee432ac886998a739f3768292f67f3ee2
OldChenGit <chenmingmao@126.com>
2021-04-30 16:45:28 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ git config --global user.name "OldChenGit"
 #仓库
 git remote add origin https://github.com/OldChenGit/HomePage.git
 #强制推送到 master 分支
-git push -u origin main
+git push -f origin master
 cd -
