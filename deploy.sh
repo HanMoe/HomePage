c89e42e4e65e8d1c44739bb511386e4dde737b6f
OldChenGit <chenmingmao@126.com>
2021-04-30 12:12:58 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+git init
+# 添加
+git add -A
+# 提交
+git commit -am"init"
+#邮箱账号
+git config --global user.email "chenmingmao@126.com"
+#用户名
+git config --global user.name "OldChenGit"
+#仓库
+git remote add origin https://github.com/OldChenGit/HomePage.git
+#强制推送到 master 分支
+git push
+cd -
